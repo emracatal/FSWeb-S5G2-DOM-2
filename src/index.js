85d6dae9e29f574565662d530c9f7514828a48cb
emracatal <134175723+emracatal@users.noreply.github.com>
2023-07-26 02:13:56 +0300

    bol eksikli ilk commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
-// document.querySelector("h1").addEventListener("click",function(e){
-// alert("Bana tıkladın!")
-// });
-
+document.querySelector("h1").addEventListener("click", function (e) {
+  alert("Bana tıkladın!");
+});
 
 // Kodlar buraya gelecek!
+
+const navA = document.querySelectorAll("nav a");
+navA.forEach((a) => {
+  a.addEventListener("mouseover", (event) => {
+    event.target.style.color = "red";
+    event.target.style.background = "yellow";
+  });
+});
